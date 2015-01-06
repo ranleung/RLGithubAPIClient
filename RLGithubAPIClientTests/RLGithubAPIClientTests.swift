74589df6e36225be7af71670fba7873c968a5ee7
ranleung <leungran@gmail.com>
2015-01-05 18:34:08 -0800

    Initial Commit

---
+++ RLGithubAPIClientTests/RLGithubAPIClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  RLGithubAPIClientTests.swift
+//  RLGithubAPIClientTests
+//
+//  Created by Randall Leung on 1/5/15.
+//  Copyright (c) 2015 Randall Leung. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RLGithubAPIClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
